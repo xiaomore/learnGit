0520c81c6bb70387d035dd95c49dc02e04db70d8
LI Heng <565793498@qq.com>
2020-10-02 17:20:47 +0800

    修改warm_up

--- learnPytorch/examples/tensors/src/warm_up.py
+++ learnPytorch/examples/tensors/src/warm_up.py
@@ -4,3 +4,10 @@
 # @FileName: warm_up.py
 # @Email : liheng00666@163.com
 # @Software: PyCharm
+
+import numpy as np
+
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+x = np.random.randn(N, D_in)
+y = np.random.randn(N, D_out)
