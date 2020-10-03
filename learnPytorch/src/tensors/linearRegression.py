d3ece88dd40558e70026761803a0b63a5ade9786
LI Heng <565793498@qq.com>
2020-10-03 17:48:03 +0800

    使用pytorch进行准备数据，训练，优化，更新参数

---
+++ learnPytorch/src/tensors/linearRegression.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/10/3 17:12
+# @Author : Heng LI
+# @FileName: linearRegression.py
+# @Software: PyCharm
+
+import torch
+
+x_data = torch.tensor([[1.0], [2.0], [3.0]])
+y_data = torch.tensor([[2.0], [4.0], [6.0]])
+
+
+class LinearModel(torch.nn.Module):
+    def __init__(self):
+        super(LinearModel, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)
+
+    def forward(self, x):
+        y_pred = self.linear(x)
+        return y_pred
+
+
+model = LinearModel()
+
+criterion = torch.nn.MSELoss(size_average=False)
+optimizer = torch.optim.ASGD(model.parameters(), lr=0.01)
+
+for epoch in range(1000):
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.item())
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+print("w = ", model.linear.weight.item())
+print("b = ", model.linear.bias.item())
+
+x_test = torch.tensor([[4.0]])
+y_test = model(x_test)
+print("y_pred = ", y_test.data)
