62ab395ed79d37ac7c0d2dae621151cde6c8a6d9
LI Heng <565793498@qq.com>
2020-10-06 22:22:54 +0800

    修改注释

--- learnPytorch/src/CNN/padding.py
+++ learnPytorch/src/CNN/padding.py
@@ -16,8 +16,8 @@ input = [3, 4, 5,
 # 输入转为张量，1, 1, 4, 3表示：batch(批处理的大小), channel(通道数，其实就是输入的矩阵数量), wight(输入的矩阵的行数), high(输入的矩阵的列数)
 input = torch.Tensor(input).view(1, 1, 4, 3)
 
-# 卷积层：1, 1表示：输入时1个通道，输出是1个通道，kernel_size是指filter矩阵的维度，padding=1是输入的填充（就是在输入矩阵最外层增加一圈，并填充为0）；bias是偏差
-conv_layer = torch.nn.Conv2d(1, 1, kernel_size=3, padding=1, bias=False)
+# 卷积层：1, 1表示：输入时1个通道，输出是1个通道，kernel_size是指filter矩阵的维度，padding=1是输入的填充（就是在输入矩阵最外层增加一圈，并填充为0）；stride是步长；bias是偏差
+conv_layer = torch.nn.Conv2d(1, 1, kernel_size=3, padding=1, stride=1, bias=False)
 
 # kernel：这里的kernel指的就是与输入矩阵做点乘的那个filter矩阵，先转为张量，在塑形为3 * 3的矩阵，1,1分别表示kernel的输入和输出的channel，3,3表示kernel为3*3
 kernel = torch.Tensor([1, 2, 3, 4, 5, 6, 7, 8, 9]).view(1, 1, 3, 3)
